59b51091923adee0e83e6ff4b59b84acb8925b88
Vansh Mhatre <vanshmhatre342006@gmail.com>
2025-12-26 23:54:16 +0530

    Improving model performance

--- Bag-of-words.py
+++ Bag-of-words.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 import numpy as np 
 import sklearn 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
 
 df = pd.read_csv("Email-classification/spam.csv")
 
@@ -8,4 +11,28 @@ df = pd.read_csv("Email-classification/spam.csv")
 
 df["spam"] = df["Category"].apply(lambda x:1 if x== "spam" else 0 )
 
-print(df[0:10])
+# print(df[0:10])
+
+train = df.drop(columns=['spam'])
+target = df["spam"]
+
+
+
+Xtrain ,Xtest , ytrain ,ytest  = train_test_split(train,target , test_size=0.2)
+
+
+
+# print(Xtrain[0:5])
+# print(Xtest[0:5])
+# print(ytrain[0:5])
+# print(ytest[0:5])
+
+# print(type(Xtrain))
+
+
+v = CountVectorizer()
+
+Xtrain_cv = v.fit_transform(Xtrain)
+
+print(type(Xtrain_cv))
+
