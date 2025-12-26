4b4b71550e23cae64f61a3d15e7e1ca0f256335a
Vansh Mhatre <git config --global user.email vanshmhatre342006@gmail.com>
2025-12-27 00:46:12 +0530

    Model created to predict the spam emails

--- Bag-of-words.py
+++ Bag-of-words.py
@@ -3,36 +3,46 @@ import numpy as np
 import sklearn 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
 
 
 df = pd.read_csv("Email-classification/spam.csv")
 
-# print(df[0:5])
-
 df["spam"] = df["Category"].apply(lambda x:1 if x== "spam" else 0 )
 
-# print(df[0:10])
-
-train = df.drop(columns=['spam'])
+train = df["Message"]
 target = df["spam"]
-
-
 
 Xtrain ,Xtest , ytrain ,ytest  = train_test_split(train,target , test_size=0.2)
 
-
-
-# print(Xtrain[0:5])
-# print(Xtest[0:5])
-# print(ytrain[0:5])
-# print(ytest[0:5])
-
-# print(type(Xtrain))
-
-
 v = CountVectorizer()
-
 Xtrain_cv = v.fit_transform(Xtrain)
 
-print(type(Xtrain_cv))
+model =MultinomialNB()
+model.fit(Xtrain_cv,ytrain)
 
+
+#### PREDICTION ON TEST DATASET ####
+
+Xtest_cv =v.transform(Xtest)
+pred = model.predict(Xtest_cv)
+
+print(classification_report(ytest,pred))
+
+
+#### EXAMPLE ####
+
+Email=[
+    "You will get 100$ in your account if you subscribe to our channel. ",
+    "Hey claim your offer before it Expires, 10000$ for sign up. ",
+    "Hello my dear , Hows you doing ?.",
+    "Hey What's up bro , meet me on monday.",
+    "Can you give me some cash around 200$."
+
+ ]
+
+Email_cv =v.transform(Email)
+pred = model.predict(Email_cv)
+
+print(pred)
